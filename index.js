0b26b5be1f23dbf221d492304f0e6f98bae18414
wuhy <sparklewhy@gmail.com>
2015-12-09 19:43:41 +0800

    add template variable type support

--- index.js
+++ index.js
@@ -5,17 +5,26 @@
 
 var path = require('path');
 
-exports.name = 'init <template> <filename>';
+exports.name = 'init [command|template] [filename] [options]';
 exports.desc = 'init project/file scaffold with the specified template type';
 exports.options = {
     '-h, --help': 'print this help message',
     '-r, --root <path>': 'set project root',
     '--force': 'force init the project and overwrite the existed file'
 };
+exports.commands = {
+    html: 'init html template',
+    js: 'init js template',
+    amd: 'init amd template',
+    spa: 'init single page application',
+    mspa: 'init mobile single page application',
+    miso: 'init mobile isomorphic application',
+    smarty: 'init smarty project'
+};
 
 exports.run = function (argv, cli, env) {
     if (argv.h || argv.help) {
-        return cli.help(exports.name, exports.options);
+        return cli.help(exports.name, exports.options, exports.commands);
     }
 
     var options = {
@@ -32,6 +41,10 @@ exports.run = function (argv, cli, env) {
         // 初始化要拷贝到的目标文件位置
         options.target = path.resolve(options.root, cmdArgs[1]);
         options.isFileTemplate = true;
+    }
+
+    if (!cmdArgs[0]) {
+        fis.log.info('Init default project template');
     }
 
     var scaffold = require('./lib/scaffold');
